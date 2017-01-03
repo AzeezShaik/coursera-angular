26b8387f0200fddb0ffedbc2f1d2112942cd6d5b
AzeezShaik <ibrahimwebtek@gmail.com>
2017-01-03 12:55:21 -0500

    Module3 solution

---
+++ module3-solution/app.js
@@ -0,0 +1,80 @@
+(function () {
+'use strict';
+
+angular.module('NarrowitDownApp', [])
+.controller('NarrowitDownController', NarrowitDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective)
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+
+NarrowitDownController.$inject = ['MenuSearchService'];
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      items: '<',
+      onRemove: '&'
+    },
+    controller: NarrowitDownController,
+    controllerAs: 'menu',
+    bindToController: true
+  };
+  return ddo;
+};
+
+
+function NarrowitDownController(MenuSearchService) {
+  var menu = this; 
+  
+  menu.searchMenuItems = function () {
+  menu.items = [];
+  if(menu.searchTerm){
+    //console.log(menu.searchTerm)
+    var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+    promise.then(function (response) {
+      menu.items = response;
+      //console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+};
+
+ menu.removeItem = function (itemIndex) {
+    menu.items.splice(itemIndex,1);
+  }
+};
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+  var items = [];
+  var foundItems = [];
+
+ service.getMatchedMenuItems = function (searchTerm) {
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")      
+    }).then (function (response){
+
+    // process result and only keep items that match
+
+    items = response.data.menu_items;
+    foundItems = [];
+
+        for (var index = 0; index < items.length; index++) {
+          if (items[index].description.indexOf(searchTerm) != -1) {
+            foundItems.push(items[index]);
+          }
+        }
+
+        // return processed items
+      return foundItems;
+    });
+  };
+}
+
+})();
